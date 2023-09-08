ebd6b59f1211bb149dda00908efa5ac8b6268f9a
seungwoo <520gow@gmail.com>
2023-09-08 11:37:52 +0900

    added logic to count words

--- scrap_app_data.py
+++ scrap_app_data.py
@@ -1,3 +1,4 @@
+from bs4 import BeautifulSoup
 import requests
 sess = requests.Session()
 adapter = requests.adapters.HTTPAdapter(max_retries = 20)
@@ -9,156 +10,142 @@ import xml.etree.ElementTree as ET
 from trafilatura import sitemaps, feeds
 import json
 import re
+from collections import Counter
 
 from kiwipiepy import Kiwi
-kiwi = Kiwi()
+kiwi = Kiwi(model_type='sbg')
 
-from transformers import BertTokenizer
-tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
+kiwi.add_user_word("리마인더", "NNG")
 
-def trim_zerospace(w):
-    newString = (w.encode('ascii', 'ignore')).decode("utf-8")
-    return newString
+def make_word_list(apps_list):
+    title_total_counts = Counter()
+    info_total_counts = Counter()
+    for app in apps_list:
+        title_total_counts += app['title_word_count']
+        info_total_counts += app['info_word_count']
+    
+    title_words_list = [key for key, _ in title_total_counts.most_common()]
+    info_words_list = [key for key, _ in info_total_counts.most_common()]
+
+    return title_words_list, info_words_list
+
+    
 
 
-def intersection(lst1, lst2):
-    lst3 = [value for value in lst1 if value in lst2]
-    return lst3
+def make_word_dict(word_list):
+    id_to_word_dict = {}
+    word_to_id_dict = {}
+    for i, word in enumerate(word_list):
+        id_to_word_dict[i] = word
+        word_to_id_dict[word] = i
+
+    return id_to_word_dict, id_to_word_dict
+    
+    
+
+
+
+# def softmax(word_counter):
+    
+    
+
+
+
+
 
 
 def clean_text(input):
     output = re.sub('[-=+,#/\?:^.@*\"※~ㆍ!』‘|\(\)\[\]`\'…》\”\“\’·]', ' ', input)
     return output
 
-
-def extract_news_url_list(url: str, container_select: str = '', list_select: str = '', filetype: str = 'html', relative_path: bool = False, host: str = ''):
+def extract_apps_url_list(url: str, container_select: str = '', list_select: str = '', filetype: str = 'html', relative_path: bool = False, host: str = ''):
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
+        "Accept-Language": "ko-kr"
     }
     origin_data = sess.get(url, headers=headers, verify=False)
-    # print(host)
-    if(filetype == 'xml'):
-        xml = BeautifulSoup(origin_data.text, 'xml')
-        url_list_container = xml.find_all(container_select)
-        url_list = []
-        for element in url_list_container:
-            if relative_path:
-                path = element.select_one(list_select).text
-                url = host + path
-                url_list.append(url)
-            else:
-                url_list.append(element.select_one(list_select).text)
-        # print(url_list)
+    html = BeautifulSoup(origin_data.text, 'html.parser')
 
-    elif(filetype == 'html'):
-        html = BeautifulSoup(origin_data.text, 'html.parser')
-        url_list_container = html.select_one(container_select)
-        # print(url_list_container)
-        url_element_list = url_list_container.select(list_select)
-        url_list = []
-        for element in url_element_list:
-            if relative_path:
-                path = element.get('href')
-                url = host + path
-                url_list.append(url)
-            else:
-                url_list.append(element.get('href'))
+    url_list_container = html.select_one(container_select)
+    url_element_list = url_list_container.select(list_select)
+    url_list = []
+    for element in url_element_list[:10]: #상위 10개 추출
+        if relative_path:
+            url_list.append(host + element.get('href'))
+        else:
+
+            url_list.append(element.get('href'))
 
     return url_list
 
-
-def extract_news(media: str, url: str, classes_to_remove: list = []):
-    print(url)
+def extract_apps_detail(url: str, title_select: str = '', title_select_p: str = '', info_select: str = ''):
     headers = {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36"
+        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/71.0.3578.98 Safari/537.36",
+        "Accept-Language": "ko-kr"
     }
-    html = sess.get(url, headers=headers, verify=False)
-    html_text = html.text
-    if(len(classes_to_remove) > 0):
-        soup = BeautifulSoup(html.text, "html.parser")
-        for class_name in classes_to_remove:
-            remove_list = soup.find_all(class_=class_name)
-            for element in remove_list:
-                element.decompose()
-        html_text = soup.prettify()
-    
-    html_extract_txt = extract(html_text, include_comments=False, output_format='json')
-    # print(html_extract_txt)
-    html_extract_json = json.loads(html_extract_txt)
-    html_extract_json["source-hostname"] = media
-    html_extract_json["source"] = url
-    html_extract_txt = json.dumps(html_extract_json)
+    origin_data = sess.get(url, headers=headers, verify=False)
+    html = BeautifulSoup(origin_data.text, 'html.parser')
+    title = html.find('title').text[:-16]
+    info = html.select_one(info_select).text
 
-    return html_extract_txt
+    return title, info
 
-def get_news_list(media: str, url: str, host: str, container_select: str = '', list_select: str = '', filetype: str = 'html', relative_url: bool = False, classes_to_remove: list = []):
-    news_url_list = extract_news_url_list(
-        url, container_select, list_select, filetype, relative_url, host)
-    news_list = []
-    for news_url in news_url_list:
-        try:
-            news = extract_news(media, news_url, classes_to_remove)
-            news_list.append(news)
-        except:
-            print('something went wrong')
-
-    return news_list
 
 
 def main():
-    ##################이곳만 수정하면 된다########################
-    today = '2023-05-12'
-    country = 'en' #kr or en
-    ##################이곳만 수정하면 된다########################
+    # 키워드 입력
+    keyword = "투두"
+    search_url = f"https://play.google.com/store/search?q={keyword}&c=apps"
 
-    news_list = []
+    # 상위 앱 추출
+    list_selector = ".fUEl2e"
+    app_selector = ".fUEl2e > div > div > div > div:nth-child(1) > a"
+    host = "https://play.google.com"
+    apps_url_list = extract_apps_url_list(search_url, container_select=list_selector, list_select=app_selector, relative_path=True, host=host)
 
-    with open(f'media_list_{country}.json', 'r', encoding='UTF-8') as f:
-        media_list = json.load(f)
-        for media in media_list:
-            print(media['media'])
-            media_news_list = get_news_list(
-                media['media'],
-                media['url'],
-                media['host'],
-                media['container_select'],
-                media['list_select'],
-                media['filetype'],
-                (media['relative_path'] == "true"),
-                media['classes_to_remove'])
+    # 앱 소개 추출
+    title_selector = "#yDmH0d > c-wiz.SSPGKf.Czez9d > div > div > div.tU8Y5c > div:nth-child(1) > div > div > c-wiz > div.hnnXjf > div.Il7kR > div > h1 > span"
+    title_selector_p = "#yDmH0d > c-wiz.SSPGKf.Czez9d > div > div > div.tU8Y5c > div.P9KVBf > div > div > c-wiz > div.hnnXjf.XcNflb.J1Igtd > div.qxNhq > div > h1 > span"
+    title_selector_p = "#yDmH0d > c-wiz.SSPGKf.Czez9d > div > div > div.tU8Y5c > div.P9KVBf > div > div > c-wiz > div.hnnXjf.XcNflb.J1Igtd > div.qxNhq > div > h1 > span"
+    info_selector ="#yDmH0d > c-wiz.SSPGKf.Czez9d > div > div > div.tU8Y5c > div.wkMJlb.YWi3ub > div > div.qZmL0 > div:nth-child(1) > c-wiz:nth-child(2) > div > section > div > div.bARER"
 
-            news_list = news_list + media_news_list
+    
+    apps_list = []
+    for url in apps_url_list:
+        print(url)
+        apps_detail = extract_apps_detail(url=url,title_select=title_selector, title_select_p= title_selector_p, info_select=info_selector)
+        title = apps_detail[0]
+        info = apps_detail[1]
+        app_info = {
+            'title': title,
+            'info': info
+        }
+        apps_list.append(app_info)
 
-        # print(f'{len(news_list)}개 뉴스 수집')
 
-    json_news_list = []
+    # 키워드 분석
 
-    for news in news_list:
-        news_json = json.loads(news)
-        if(news_json['date'] != today):
-            continue
+    ##키워드 카운트
+    allowed_tags = {'NNG', 'NNP', 'VV'}
+    for app in apps_list:
+        app['title_word_count'] = Counter([x.form for x in kiwi.tokenize(clean_text(app['title'])) if x.tag in allowed_tags])
+        app['info_word_count'] = Counter([x.form for x in kiwi.tokenize(clean_text(app['info'])) if x.tag in allowed_tags])
+    
+    ##전체 word list 및 word dictionary 생성
+    title_word_list, info_word_list = make_word_list(apps_list=apps_list)
+    title_id_to_word_dict, title_word_to_id_dict = make_word_dict(title_word_list)
+    info_id_to_word_dict, info_word_to_id_dict = make_word_dict(info_word_list)
 
-        if(any(x for x in json_news_list if x['title'] == news_json['title'])):
-            continue
+    
 
-        if(country == 'kr'):
-            token = kiwi.tokenize(clean_text(news_json['raw_text']))
-            morph = [item.form for item in token if item.tag == 'NNG' or item.tag ==
-                    'NNP' or item.tag == 'NNB' or item.tag == 'NR' or item.tag == 'NP' or item.tag == 'SN']
-            morph = set(morph)
-            news_json['token_set'] = list(morph)
-            json_news_list.append(news_json)
-        elif(country == 'en'):
-            token = tokenizer.tokenize(clean_text(news_json['raw_text']))
-            morph = set(token)
-            news_json['token_set'] = list(morph)
-            json_news_list.append(news_json)
-        # print(news['title'])
-    print(f'{len(json_news_list)}개 뉴스 수집')
+    print(info_word_to_id_dict)
 
-    with open(f'news_list_json_new/{today}_{country}.json', 'w', encoding='UTF-8') as f:
-        json.dump(json_news_list, f, indent=2, ensure_ascii=False)
 
+
+
+    
+
+        
 
 if __name__ == "__main__":
     main()
